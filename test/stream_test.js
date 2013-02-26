6a43e6c2dfa1a18be594f75b331b802c154e938c
Raul Jara <raul.c.jara@gmail.com>
2013-02-26 11:07:30 -0500

    Add passing test for filter

--- test/stream_test.js
+++ test/stream_test.js
@@ -1,24 +1,32 @@
 var expect = require('expect.js')
-  , stream = require('../lib/stream.js')
+  , s = require('../lib/stream.js')
   ;
 
 describe('stream', function() {
   describe('nats', function() {
     it('has a function "next"', function() {
-      expect( typeof stream.nats().next )
+      expect( typeof s.nats().next )
             .to.equal('function');
     });
 
     it('contains a val with the first natural number', function() {
-      expect(stream.nats().val)
+      expect( s.nats().val )
             .to.equal(1);
     });
   });
 
   describe('take', function() {
     it('produces an array from a stream', function() {
-      expect( stream.take(5, stream.nats) )
+      expect( s.take(5, s.nats) )
             .to.eql([1, 2, 3, 4, 5]);
+    });
+  });
+
+  describe('filter', function() {
+    it('produces a filtered stream', function() {
+      var isEven = function(i) { return i % 2 == 0; };
+      expect( s.take(5, s.filter(isEven, s.nats)) )
+            .to.eql([2, 4, 6, 8, 10]);
     });
   });
 });
